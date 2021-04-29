be2e2ff5372cc43aebfb89698d910448cc56ddf8
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-04-29 14:13:53 +0200

    Delete duplicated function

--- R/infoworks_gerris_functions.R
+++ R/infoworks_gerris_functions.R
@@ -2,61 +2,6 @@
 ############ functions needed for the infoworks-Gerris-Interface ###############
 ################################################################################
 
-# 1. loading inworks Output ----------------------------------------------------
-#' Title
-#'
-#' @param iw_output_folder
-#' @param simulation_name
-#'
-#' @return
-#' @export
-#'
-#' @examples
-load_infoworks_output <- function(
-  iw_output_folder,
-  simulation_name
-){
-  # output parameters from parameter_conversion table
-  output_paras <-
-    parameter_conversion[
-      !is.na(parameter_conversion[,"id_infoworks"]),"id_infoworks"]
-
-  # filter for the right files
-  output_files <- list.files(path = iw_output_folder)
-  output_files <- grep(pattern = simulation_name, x = output_files, value = T)
-  if(length(output_files) == 0){
-    stop(paste("no Infoworks data named", simulation_name))
-  }
-  output_files <- grep(pattern = paste0("\\.csv$"), x = output_files, value = T)
-  output_files <- grep(pattern = paste0("^Link"), x = output_files, value = T)
-  output_files <- grep(pattern = paste0("_ds_"), x = output_files, value = T)
-
-  # load infoworks output data
-  iw_out <- list()
-  for(output_para in output_paras){
-    this_file <- grep(pattern = output_para, x = output_files, value = T)
-    if(length(this_file) == 0){
-      warning("no data-file for parameter: ", output_para)
-    } else {
-      iw_out[[output_para]] <-
-        read.csv(file = paste0(iw_output_folder, this_file),
-                 header = T, sep = ",", dec = ".", stringsAsFactors = F)
-
-      # only keep links listed in the outlet_conversion table
-      ids <- ids_from_colnames(COLnames = colnames(iw_out[[output_para]]))
-      missing_outlets <- which(!(outlet_conversion$upstream_link_id %in% ids))
-      keep_those <- which(ids %in% outlet_conversion$upstream_link_id)
-      iw_out[[output_para]] <- iw_out[[output_para]][c(1,2,keep_those)]
-
-      message(output_para, " loaded")
-    }
-  }
-  if(length(missing_outlets) > 0){
-    warning(paste0("the following links are listed in the conversion table but not found in Infowokrs output: ",
-                   outlet_conversion$upstream_link_id[missing_outlets]))
-  }
-  iw_out
-}
 # 2. Flow correction -----------------------------------------------------------
 correct_flow <- function(
   input_data,
